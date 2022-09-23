3b2e472fdd20424cfaf0ad1234fb2af5a8616bfb
valon <valonloshaj@gmail.com>
2022-09-23 15:24:20 -0400

    initial commit

---
+++ slds-demo/main/default/lwc/fieldInputCustom/fieldInputCustom.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class FieldInputCustom extends LightningElement {
+    handleInputFocus(event) {
+        // modify parent to properly highlight visually
+        const classList = event.target.parentNode.classList;
+        classList.add('lgc-highlight');
+    }
+
+    handleInputBlur(event) {
+        // modify parent to properly remove highlight
+        const classList = event.target.parentNode.classList;
+        classList.remove('lgc-highlight');
+    }
+
+    handleInputChange(event) {
+        this.textValue = event.detail.value;
+    }
+}
---
+++ slds-demo/main/default/lwc/fieldInputSlds/fieldInputSlds.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class InputTextAdvanced extends LightningElement {
+    textValue;
+
+    handleInputFocus(event) {
+        // modify parent to properly highlight visually
+        const classList = event.target.parentNode.classList;
+        classList.add('lgc-highlight');
+    }
+
+    handleInputBlur(event) {
+        // modify parent to properly remove highlight
+        const classList = event.target.parentNode.classList;
+        classList.remove('lgc-highlight');
+    }
+
+    handleInputChange(event) {
+        this.textValue = event.detail.value;
+    }
+}
